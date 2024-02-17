7b0bf079b0fd12104ba08f8fbd3dc1889f9891ef
emfallah <emfallah@digitalvirgo.com>
2024-02-17 18:08:00 +0100

    Feature: User profile update image form

--- src/Security/Voter/MicroPostVoter.php
+++ src/Security/Voter/MicroPostVoter.php
@@ -2,60 +2,65 @@
 
 namespace App\Security\Voter;
 
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Authorization\Voter\Voter;
-use Symfony\Component\Security\Core\User\UserInterface;
+use App\Entity\User;
 use App\Entity\MicroPost;
-use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
 class MicroPostVoter extends Voter
 {
-    public const EDIT = 'POST_EDIT';
-    public const VIEW = 'POST_VIEW';
-    public const COMMENT = 'POST_COMMENT';
-
     public function __construct(
         private Security $security
-    )
-    {
-        
+    ) {
     }
 
-    protected function supports(string $attribute, mixed $subject): bool
+    protected function supports(string $attribute, $subject): bool
     {
-        return in_array($attribute, [self::EDIT, self::VIEW])
+        // replace with your own logic
+        // https://symfony.com/doc/current/security/voters.html
+        return in_array($attribute, [MicroPost::EDIT, MicroPost::VIEW])
             && $subject instanceof MicroPost;
     }
 
     /**
-     * @param Micropost $subject
+     * @param MicroPost $subject
      */
-    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
-    {
-        $isAdmin = $this->security->isGranted('ROLE_ADMIN');
-        if ($isAdmin === true) {
+    protected function voteOnAttribute(
+        string $attribute,
+        $subject,
+        TokenInterface $token
+    ): bool {
+        /** @var User $user  */
+        $user = $token->getUser();
+        // if the user is anonymous, do not grant access
+        // if (!$user instanceof UserInterface) {
+        //     return false;
+        // }
+        $isAuth = $user instanceof UserInterface;
+
+        if ($this->security->isGranted('ROLE_ADMIN')) {
             return true;
         }
 
-        /** @var User $user */
-        $user = $token->getUser();
-
-        $isAuth = $user instanceof UserInterface;
-
+        // ... (check conditions and return true to grant permission) ...
         switch ($attribute) {
-            case self::EDIT:
-                return $isAuth && (
-                    $subject->getAuthor()->getId() == $user->getId() || $isAdmin === true);
-                break;
+            case MicroPost::EDIT:
+                return $isAuth
+                    && (
+                        ($subject->getAuthor()->getId() === $user->getId()) ||
+                        $this->security->isGranted('ROLE_EDITOR')
+                    );
+            case MicroPost::VIEW:
+                if (!$subject->isExtraPrivacy()) {
+                    return true;
+                }
 
-            case self::VIEW:
-                return true;
-                break;
-
-            case self::COMMENT:
-                return $isAuth && (
-                    $subject->getAuthor()->isVerified() === true);
-                break;
+                return $isAuth &&
+                    ($subject->getAuthor()->getId() === $user->getId()
+                        || $subject->getAuthor()->getFollows()->contains($user)
+                    );
         }
 
         return false;
